d9fdbebb59c3e9e48bce97b8a7e1dea79d836349
alexchen <alexchenunix@gmail.com>
2026-09-22 00:31:35 +0000

    update dockerfile

--- rebuild_micro_ros_lib.sh
+++ rebuild_micro_ros_lib.sh
@@ -1,2 +1,11 @@
+if [ ! -d /opt/uros_ws/ ]; then
+    echo "Micro-ROS workspace not found. Please run the script in a Micro-ROS workspace."
+    echo "Install ros2 base system and micro-ros-setup package, then run the following commands:"
+    echo "  source /opt/uros_ws/install/setup.bash"
+    echo "  bash libs/micro_ros/microros_static_library/library_generation/library_generation.sh"
+    echo "  bash libs/micro_ros_arduino/extras/library_generation/library_generation.sh coral_micro_wifi"
+    exit 1
+fi
+
 bash libs/micro_ros/microros_static_library/library_generation/library_generation.sh
 bash libs/micro_ros_arduino/extras/library_generation/library_generation.sh coral_micro_wifi
